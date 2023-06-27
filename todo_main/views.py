7b607ccf8cb30019593c469df1092cad1eed7ff8
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-06-27 22:21:16 +0300

    Fetch Task from Model and print in Frontend

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from todo.models import Task
 
 
 def home(request):
-    return render(request, 'home.html')
+    tasks = Task.objects.filter(is_competed=False)
+    
+    context = {
+        'tasks': tasks
+    }
+    
+    return render(request, 'home.html', context=context)
